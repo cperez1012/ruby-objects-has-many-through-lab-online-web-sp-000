e4199686ef869f34ac242e56779b613a3e2cbfba
Christian Perez <perezchristian1012@gmail.com>
2019-09-10 11:41:24 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,14 +13,14 @@ class Doctor
     @@all
   end
 
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+  end
+
   def appointments
     Appointment.all.select do |appointment|
       appointment.doctor == self
     end
-  end
-
-  def new_appointment(date, patient)
-    Appointment.new(date, patient, self)
   end
 
   def patients
@@ -28,4 +28,5 @@ class Doctor
       appointment.patient
     end
   end
+
 end
